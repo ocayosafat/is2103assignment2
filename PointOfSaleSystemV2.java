25141920d516bff238103ee2a5c463d268d15faa
Kitx2 <kitcrusher@gmail.com>
2018-03-20 16:52:36 +0800

    Add files via upload

---
+++ PointOfSaleSystemV2.java
@@ -0,0 +1,18 @@
+package pointofsalesystemv2;
+
+
+
+public class PointOfSaleSystemV2
+{
+    public PointOfSaleSystemV2()
+    {
+    }
+    
+    
+    
+    public static void main(String[] args) 
+    {
+        MainApp mainApp = new MainApp();
+        mainApp.runApp();
+    }   
+}
