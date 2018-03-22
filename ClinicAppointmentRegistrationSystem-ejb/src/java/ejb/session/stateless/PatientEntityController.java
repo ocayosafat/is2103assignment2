9387d72b2590fe8c81c6e418539b9e9870c05893
adenchua <37574101+adenchua@users.noreply.github.com>
2018-03-22 18:44:17 +0800

    Add files via upload

--- ClinicAppointmentRegistrationSystem-ejb/src/java/ejb/session/stateless/PatientEntityController.java
+++ ClinicAppointmentRegistrationSystem-ejb/src/java/ejb/session/stateless/PatientEntityController.java
@@ -5,10 +5,90 @@
  */
 package ejb.session.stateless;
 
+import entity.PatientEntity;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 /**
- *
+ * Version 1.00
  * @author Aden
  */
-public class PatientEntityController {
+
+@Stateless
+@Local(PatientEntityControllerLocal.class)
+@Remote(PatientEntityControllerRemote.class)
+
+public class PatientEntityController implements PatientEntityControllerLocal, PatientEntityControllerRemote {
+    
+    @PersistenceContext(unitName = "ClinicAppointmentRegistrationSystem")
+    private EntityManager em;
+    
+    public PatientEntityController() {
+        
+    }
+    
+    @Override
+    public PatientEntity createNewPatient(PatientEntity newPatientEntity)
+    {
+        em.persist(newPatientEntity);
+        em.flush();
+        
+        return newPatientEntity;
+    }
+    
+    @Override
+    public List<PatientEntity> retrieveAllPatients()
+    {
+        Query query = em.createQuery("SELECT p FROM PatientEntity p");
+        
+        return query.getResultList();
+    }
+    
+    @Override
+    public PatientEntity retrievePatientByPatientId(Long PatientId) throws PatientNotFoundException
+    {
+        PatientEntity patientEntity = em.find(PatientEntity.class, patientId);
+        
+        if(patientEntity != null)
+        {
+            return patientEntity;
+        }
+        else
+        {
+            throw new PatientNotFoundException("PatientID " + patientId + " does not exist!");
+        }
+    }
+    
+    @Override
+    public PatientEntity retrievePatientByPatientIdentityNumber(String identityNumber) throws PatientNotFoundException
+    {
+        PatientEntity patientEntity = em.find(PatientEntity.class, identityNumber);
+        
+        if(patientEntity != null)
+        {
+            return patientEntity;
+        }
+        else
+        {
+            throw new PatientNotFoundException("PatientID " + patientId + " does not exist!");
+        }
+    }
+    
+    @Override
+    public void updatePatient(PatientEntity patientEntity)
+    {
+        em.merge(patientEntity);
+    }
+    
+    
+    @Override
+    public void deletePatient(String identityNumber) throws PatientNotFoundException
+    {
+        PatientEntity patientEntityToRemove = retrievePatientByPatientIdentityNumber(identityNumber);
+        em.remove(patientEntityToRemove);
+    }
+    
     
 }
--- ClinicAppointmentRegistrationSystem-ejb/src/java/ejb/session/stateless/StaffEntityController.java
+++ ClinicAppointmentRegistrationSystem-ejb/src/java/ejb/session/stateless/StaffEntityController.java
@@ -5,10 +5,121 @@
  */
 package ejb.session.stateless;
 
+import entity.StaffEntity;
+import java.util.List;
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import util.exception.StaffNotFoundException;
+
 /**
- *
+ * Version: 1.00
  * @author Aden
  */
-public class StaffEntityController {
+
+@Stateless
+@Local(StaffEntityControllerLocal.class)
+@Remote(StaffEntityControllerRemote.class)
+
+public class StaffEntityController implements StaffEntityControllerLocal, StaffEntityControllerRemote {
+    
+    @PersistenceContext(unitName = "ClinicAppointmentRegistrationSystem")
+    private EntityManager em;
+
+    public StaffEntityController() {
+        
+    }
+    
+    @Override
+    public StaffEntity createNewStaff(StaffEntity newStaffEntity)
+    {
+        em.persist(newStaffEntity);
+        em.flush();
+        
+        return newStaffEntity;
+    }
+    
+    @Override
+    public List<StaffEntity> retrieveAllStaffs()
+    {
+        Query query = em.createQuery("SELECT s FROM StaffEntity s");
+        
+        return query.getResultList();
+    }
+    
+    @Override
+    public StaffEntity retrieveStaffByStaffId(Long staffId) throws StaffNotFoundException
+    {
+        StaffEntity staffEntity = em.find(StaffEntity.class, staffId);
+        
+        if(staffEntity != null)
+        {
+            return staffEntity;
+        }
+        else
+        {
+            throw new StaffNotFoundException("Staff ID " + staffId + " does not exist!");
+        }
+    }
+    
+    @Override
+    public StaffEntity retrieveStaffByUsername(String username) throws StaffNotFoundException
+    {
+        Query query = em.createQuery("SELECT s FROM StaffEntity s WHERE s.username = :inUsername");
+        query.setParameter("inUsername", username);
+        
+        try
+        {
+            return (StaffEntity)query.getSingleResult();
+        }
+        catch(NoResultException | NonUniqueResultException ex)
+        {
+            throw new StaffNotFoundException("Staff Username " + username + " does not exist!");
+        }
+    }
+    
+    @Override
+    public StaffEntity staffLogin(String username, String password) throws InvalidLoginException
+    {
+        try
+        {
+            StaffEntity staffEntity = retrieveStaffByUsername(username);
+            
+            if(staffEntity.getPassword().equals(password))
+            {
+                return staffEntity;
+            }
+            else
+            {
+                throw new InvalidLoginException("Username does not exist or invalid password!");
+            }
+        }
+        catch(StaffNotFoundException ex)
+        {
+            throw new InvalidLoginException("Username does not exist or invalid password!");
+        }
+    }
+    
+    @Override
+    public void updateStaff(StaffEntity staffEntity)
+    {
+        em.merge(staffEntity);
+    }
+    
+    
+    @Override
+    public void deleteStaff(Long staffId) throws StaffNotFoundException
+    {
+        StaffEntity staffEntityToRemove = retrieveStaffByStaffId(staffId);
+        em.remove(staffEntityToRemove);
+    }
+    
+    
+    
     
 }
