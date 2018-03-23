26e0110e86ac17aefdaac9676d4525f33a15a0a0
adenchua <37574101+adenchua@users.noreply.github.com>
2018-03-23 14:29:03 +0800

    Add files via upload

--- ClinicAppointmentRegistrationSystem-ejb/src/java/ejb/session/stateless/StaffEntityControllerLocal.java
+++ ClinicAppointmentRegistrationSystem-ejb/src/java/ejb/session/stateless/StaffEntityControllerLocal.java
@@ -5,10 +5,30 @@
  */
 package ejb.session.stateless;
 
+import entity.StaffEntity;
+import java.util.List;
+import util.exception.InvalidLoginException;
+import util.exception.StaffNotFoundException;
+
 /**
- *
+ * Version 1.0
  * @author Aden
  */
-public class StaffEntityControllerLocal {
+public interface StaffEntityControllerLocal {
+    
+    StaffEntity createNewStaff(StaffEntity newStaffEntity);
+    
+    List<StaffEntity> retrieveAllStaffs();
+    
+    StaffEntity retrieveStaffByStaffId(Long staffId) throws StaffNotFoundException;
+    
+    StaffEntity retrieveStaffByUsername(String username) throws StaffNotFoundException;
+    
+    StaffEntity staffLogin(String username, String password) throws InvalidLoginException;
+    
+    void updateStaff(StaffEntity staffEntity);
+    
+    void deleteStaff(Long staffId) throws StaffNotFoundException;
+    
     
 }
