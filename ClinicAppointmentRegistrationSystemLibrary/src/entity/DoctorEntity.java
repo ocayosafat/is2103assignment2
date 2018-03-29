d6f8ee14c19f2de36ae41f89beb3419562880750
Kitx2 <kitcrusher@gmail.com>
2018-03-29 12:25:56 +0800

    Add files via upload

--- ClinicAppointmentRegistrationSystemLibrary/src/entity/DoctorEntity.java
+++ ClinicAppointmentRegistrationSystemLibrary/src/entity/DoctorEntity.java
@@ -7,6 +7,7 @@ package entity;
 
 import java.io.Serializable;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,6 +16,7 @@ import javax.persistence.Id;
  * Version: 1.00
  * @author Aden
  */
+@Entity
 public class DoctorEntity implements Serializable {
     
     private static final long serialVersionUID = 1L;
